3a3d2e0960d4deb6d4b60d25b17d56caa71446ad
Gyubin-Han <yangglei2@naver.com>
2024-10-09 15:27:13 +0900

    first commit

---
+++ src/main/java/be/gyu/nhnacademy/jpa_final/JpaFinalApplication.java
@@ -0,0 +1,13 @@
+package be.gyu.nhnacademy.jpa_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/gyu/nhnacademy/jpa_final/JpaFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gyu.nhnacademy.jpa_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
