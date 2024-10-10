018f9a4d48ce3222e0c6e81457bcf8c4982ced81
Gyubin-Han <yangglei2@naver.com>
2024-10-10 17:15:56 +0900

    fix: Change the response body

--- src/main/java/com/nhnacademy/minidooray/account/controller/MemberController.java
+++ src/main/java/com/nhnacademy/minidooray/account/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.nhnacademy.minidooray.account.controller;
 import com.nhnacademy.minidooray.account.dto.MemberDto;
 import com.nhnacademy.minidooray.account.entity.Member;
 import com.nhnacademy.minidooray.account.service.MemberService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,13 +16,14 @@ public class MemberController {
     }
 
     @GetMapping("/member/{memberId}")
-    public Member getMember(@PathVariable String memberId){
-        return memberService.getMember(memberId);
+    public ResponseEntity<Member> getMember(@PathVariable String memberId){
+        return new ResponseEntity<>(memberService.getMember(memberId),HttpStatus.OK);
     }
 
     @PostMapping("/member")
     public ResponseEntity<String> registerMember(@RequestBody MemberDto dto){
         memberService.registerMember(dto);
-        return ResponseEntity.ok().build();
+
+        return new ResponseEntity<>("저장성공", HttpStatus.OK);
     }
 }
